8b7cf491f5430307602cbed93cafba69f6e98857
Muhammad Azamuddin <muhammad.azamuddin@kemenkeu.go.id>
2016-06-01 17:07:30 +0700

    Fix previous commit error

--- public/js/src/components/RowTalent.react.js
+++ public/js/src/components/RowTalent.react.js
@@ -67,23 +67,23 @@ var RowTalent = React.createClass({
         kuadran = 0;
 
         // console.log(this.props.nama, this.props.rangeKompetensi, this.props.rangeNKP)
-        if(item.rangeKompetensi == "tinggi" && item.rangeNKP == "tinggi"){
+        if(this.props.rangeKompetensi == "tinggi" && this.props.rangeNKP == "tinggi"){
             kuadran = 9
-        } else if(item.rangeKompetensi == "sedang" && item.rangeNKP == "tinggi"){
+        } else if(this.props.rangeKompetensi == "sedang" && this.props.rangeNKP == "tinggi"){
             kuadran = 8
-        } else if(item.rangeKompetensi == "rendah" && item.rangeNKP == "tinggi"){
+        } else if(this.props.rangeKompetensi == "rendah" && this.props.rangeNKP == "tinggi"){
             kuadran = 5
-        } else if(item.rangeKompetensi == "tinggi" && item.rangeNKP == "sedang"){
+        } else if(this.props.rangeKompetensi == "tinggi" && this.props.rangeNKP == "sedang"){
             kuadran = 7
-        } else if(item.rangeKompetensi == "sedang" && item.rangeNKP == "sedang"){
+        } else if(this.props.rangeKompetensi == "sedang" && this.props.rangeNKP == "sedang"){
             kuadran = 6
-        } else if(item.rangeKompetensi == "rendah" && item.rangeNKP == "sedang"){
+        } else if(this.props.rangeKompetensi == "rendah" && this.props.rangeNKP == "sedang"){
             kuadran = 4
-        } else if(item.rangeKompetensi == "tinggi" && item.rangeNKP == "rendah"){
+        } else if(this.props.rangeKompetensi == "tinggi" && this.props.rangeNKP == "rendah"){
             kuadran = 3
-        } else if(item.rangeKompetensi == "sedang" && item.rangeNKP == "rendah"){
+        } else if(this.props.rangeKompetensi == "sedang" && this.props.rangeNKP == "rendah"){
             kuadran = 2
-        } else if(item.rangeKompetensi == "rendah" && item.rangeNKP == "rendah"){
+        } else if(this.props.rangeKompetensi == "rendah" && this.props.rangeNKP == "rendah"){
             kuadran = 1
         }
 
